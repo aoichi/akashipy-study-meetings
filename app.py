1c89a3abda3c3f202d9641d0f3d45795c200f18c
aoichi <sososososochan@gmail.com>
2015-07-05 17:14:19 +0900

    herokuの準備

--- app.py
+++ app.py
@@ -10,5 +10,8 @@ def events():
         events=get_events(keyword),
         keyword=keyword
     )
-
-run(host='localhost', port=8080, debug=True, reloader=True)
+from os import environ
+if environ['HEROKU']:
+    run(host='0.0.0.0', port=int(environ.get('PORT', 5000)))
+else:
+    run(host='localhost', port=8080, debug=True, reloader=True)
