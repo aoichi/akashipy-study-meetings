7694522377bb2db593b0f4864a204473e80c65bc
YuZakuro <zakuro@yuzakuro.me>
2015-07-05 16:00:23 +0900

    検索キーワードをqueryから指定できるように

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from bottle import route, run, template
+from bottle import route, run, template, request
 from events import get_events
 
 
 @route('/events')
 def events():
-    return template('events', events=get_events('python'))
+    return template('events', events=get_events(request.query.keyword))
 
 run(host='localhost', port=8080, debug=True, reloader=True)
