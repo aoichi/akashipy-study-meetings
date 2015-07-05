fb8e0c5d60013c7bba0f795a5c72c90a05ea134f
YuZakuro <zakuro@yuzakuro.me>
2015-07-05 14:58:17 +0900

    余分なビューを削除

--- app.py
+++ app.py
@@ -1,16 +1,6 @@
 from bottle import route, run, template
 
 
-@route('/')
-def index():
-    return '<b>Hello World!</b>'
-
-
-@route('/hello')
-def hello():
-    return template('Hello', name='aoichi')
-
-
 @route('/events')
 def events():
     return template('Hello', name='aoichi')
